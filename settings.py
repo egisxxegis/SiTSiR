faf08750c94b9c1e1a3f6dbcca0d8e6a8e48a8c5
user0481 <egisxxegis@gmail.com>
2022-05-24 11:59:19 +0300

    Remove redundant settings

--- settings.py
+++ settings.py
@@ -1,9 +1,4 @@
 class Settings(object):
-    dbHost = 'localhost'
-    dbName = 'sp2b_rdf2rdb_clean'
-    dbUser = 'root'
-    dbPassword = ''
-
     queryInputFile = "query.sparql"
     queryOutputFile = "query.sql"
     rdf2rdbMapFile = "unpacked_map_sp2b_rdf2rdb_clean.yaml"
