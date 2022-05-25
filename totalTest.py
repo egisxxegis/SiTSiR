d8295f23309313a29fc92cb42daa734531886404
user0481 <egisxxegis@gmail.com>
2022-05-25 11:09:28 +0300

    show what queries are supported and what are not yet

---
+++ totalTest.py
@@ -0,0 +1,41 @@
+import yaml
+
+from SPARQL.parser import Parser
+from SPARQL.queryFabric import QueryFabric
+from settings import Settings
+from translate.translator import Translator
+
+with open(Settings.rdf2rdbMapFile, 'r') as mapFile:
+    rdf2rdb = yaml.unsafe_load(mapFile)
+
+inputs = [f"test_data/query0{x}.sparql" for x in [1, 2, "3a", "3b", 4, "5a", "5b"]]
+for sparqlIn in inputs:
+    print(f"Translating {sparqlIn}")
+    with open(sparqlIn, 'r') as queryInputFile:
+        queryRaw = queryInputFile.read()
+    parser = Parser(queryRaw)
+    queryFabric = QueryFabric()
+    query = queryFabric.query_from_parser(parser)
+    translator = Translator(query)
+    sql_as_string = translator.translate(rdf2rdb)
+
+illegal_inputs = [f"test_data/query{x}.sparql" for x in ["06", "07", "08", "09", "10", "11"]]
+loglog = []
+for sparqlIn in illegal_inputs:
+    print(f"Translating {sparqlIn}")
+    loglog.append(f"Translating {sparqlIn}")
+    with open(sparqlIn, 'r') as queryInputFile:
+        queryRaw = queryInputFile.read()
+    parser = Parser(queryRaw)
+    queryFabric = QueryFabric()
+    query = queryFabric.query_from_parser(parser)
+    translator = Translator(query)
+    try:
+        sql_as_string = translator.translate(rdf2rdb)
+    except Exception:
+        print(f"Translating {sparqlIn} FAILED")
+        loglog.append(f"Translating {sparqlIn} FAILED")
+
+print("-----------------")
+for log in loglog:
+    print(log)
